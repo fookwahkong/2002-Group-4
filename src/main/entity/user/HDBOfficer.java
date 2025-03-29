ab78be721180a4825e8933b300beaceb04422172
Mau-Ze-Ming <zmau001@e.ntu.edu.sg>
2025-03-29 23:26:14 +0800

    Update HDBOfficer.java

--- src/main/entity/user/HDBOfficer.java
+++ src/main/entity/user/HDBOfficer.java
@@ -8,4 +8,23 @@ public class HDBOfficer extends Applicant {
     public HDBOfficer(String userID, String password, String name, int age, MaritalStatus maritalStatus, UserRole userRole) {
         super(userID, password, name, age, maritalStatus, userRole);
     }
+
+    public static List<Enquiry> getEnquiriesByOfficer(HDBOfficer officer) {
+        List<Enquiry> officerEnquiries = new ArrayList<>();
+        
+        for (Project project : ProjectController.getOfficerProjects(officer)) {
+            officerEnquiries.addAll(project.getEnquiriesHandledByOfficer(officer));
+        }
+        return officerEnquiries;
+    }
+
+    public static void replyToEnquiry(Enquiry enquiry, String response) {
+        if (enquiry == null) {
+            System.out.println("Invalid enquiry.");
+            return;
+        }
+        enquiry.setResponse(response);
+        enquiry.setReplied(true);  
+        System.out.println("Response submitted: " + response);
+    }
 }

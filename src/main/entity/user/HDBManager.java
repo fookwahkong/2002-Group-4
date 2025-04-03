925e72a0cc81769fd0a5df42f2ed3df0cdf885c5
Mau-Ze-Ming <zmau001@e.ntu.edu.sg>
2025-04-04 00:08:44 +0800

    added approveOfficer, getPendingApprovals and addPendingApprovals

--- src/main/entity/user/HDBManager.java
+++ src/main/entity/user/HDBManager.java
@@ -4,8 +4,22 @@ import main.enums.MaritalStatus;
 import main.enums.UserRole;
 
 public class HDBManager extends User{
-
+    private List<HDBOfficer> pendingApprovals = new ArrayList<>();
     public HDBManager(String userID, String password, String name, int age, MaritalStatus maritalStatus, UserRole userRole) {
         super(userID, password, name, age, maritalStatus, userRole);
+    }
+
+    public List<HDBOfficer> getPendingApprovals() {
+        return pendingApprovals;
+    }
+
+    public void addPendingApproval(HDBOfficer officer) {
+        pendingApprovals.add(officer);
+    }
+
+    public void approveOfficer(HDBOfficer officer) {
+        officer.approve();
+        pendingApprovals.remove(officer);
+        ProjectController.addApprovedOfficer(officer);
     }
 }

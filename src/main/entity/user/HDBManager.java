db403c07ff32e911844e6f7077f3245be929db9c
Mau-Ze-Ming <zmau001@e.ntu.edu.sg>
2025-04-05 16:39:00 +0800

    Update HDBManager.java

--- src/main/entity/user/HDBManager.java
+++ src/main/entity/user/HDBManager.java
@@ -21,7 +21,6 @@ public class HDBManager extends User{
     public void approveOfficer(HDBOfficer officer, Project project) {
         if (project.getPendingOfficers().contains(officer)) {
             officer.assignProject(project);
-            project.getApprovedOfficers().add(officer);
             project.addOfficersInCharge(officer);
             project.getPendingOfficers().remove(officer);
             System.out.println(officer.getName() + " has been approved for project " + project.getName());

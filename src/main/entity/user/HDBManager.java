85f1f6594bed1461980abb7052e4d9589526959f
Kris <kris.khor.hai.xiang@gmail.com>
2025-04-06 01:44:45 +0800

    Added a project import into the HDBManager Class. Allows for my test case to run and pass.

--- src/main/entity/user/HDBManager.java
+++ src/main/entity/user/HDBManager.java
@@ -1,15 +1,18 @@
 package main.entity.user;
 
+import main.entity.project.Project;
 import main.enums.MaritalStatus;
 import main.enums.UserRole;
 
 public class HDBManager extends User {
+
     private Project currentProject;
+
     public HDBManager(String userID, String password, String name, int age, MaritalStatus maritalStatus, UserRole userRole) {
         super(userID, password, name, age, maritalStatus, userRole);
     }
 
-    public void setCurrentProject (Project project) {
+    public void setCurrentProject(Project project) {
         this.currentProject = project;
     }
 

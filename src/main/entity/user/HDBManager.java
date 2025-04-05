15b56e25ea5ed1b4663c8bda262a72b89da7bf63
Mau-Ze-Ming <zmau001@e.ntu.edu.sg>
2025-04-05 20:55:59 +0800

    added get and set current projects for manager

--- src/main/entity/user/HDBManager.java
+++ src/main/entity/user/HDBManager.java
@@ -4,8 +4,16 @@ import main.enums.MaritalStatus;
 import main.enums.UserRole;
 
 public class HDBManager extends User {
-
+    private Project currentProject;
     public HDBManager(String userID, String password, String name, int age, MaritalStatus maritalStatus, UserRole userRole) {
         super(userID, password, name, age, maritalStatus, userRole);
+    }
+
+    public void setCurrentProject (Project project) {
+        this.currentProject = project;
+    }
+
+    public Project getCurrentProject() {
+        return this.currentProject;
     }
 }
